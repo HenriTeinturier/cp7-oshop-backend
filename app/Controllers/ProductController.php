8a33da0e70104605c76c50d144902761700faa3c
HenriTeinturier <henri.teinturier@gmail.com>
2022-02-15 14:51:19 +0100

    dynamisation page produit ok

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Models\Product;
+
 // Si j'ai besoin du Model Category
 // use App\Models\Category;
 
@@ -14,10 +16,17 @@ class ProductController extends CoreController
      */
     public function productAction()
     {
+        $productsModel = new Product;
+        $products = $productsModel->findAll();
+
+        $data = [
+            'products'=> $products,
+        ];
+
         // On appelle la méthode show() de l'objet courant
         // En argument, on fournit le fichier de Vue
         // Par convention, chaque fichier de vue sera dans un sous-dossier du nom du Controller
-        $this->show('product/product');
+        $this->show('product/product',$data);
     }
 
     /**
